2b82d21aa2b64bbb58a1af5ffccccea2cf96b772
Swaff <swaff68@gmail.com>
2014-06-30 18:00:23 -0600

    finished part 1

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 $(document).on('ready', function() {
+
+$('#map').click(function(){
+	$( document ).on( "mousemove", function( event ) {
+ 	 $( "#log" ).text( "pageX: " + event.pageX + ", pageY: " + event.pageY );
+ 	 console.log("pageX: " + event.pageX + ", pageY: " + event.pageY);
+	});
+
+	$('<div> <img class="icon" src="gold rush marker.png"></div>').attr('class','newIcon').appendTo('#map').css({
+		'position': 'absolute',
+		'top': event.pageY - 40,
+		'left': event.pageX - 19
+
+	});
+
+
+
+});
+
+
+
   
 });
